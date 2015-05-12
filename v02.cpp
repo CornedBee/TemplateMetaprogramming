1af112eea5c60c91188105ec129a6006f4c944d8
CornedBee <sebastian.redl@getdesigned.at>
2015-05-12 08:07:33 -0600

    Add emptyCase, but cheat on the implementation.

---
+++ v02.cpp
@@ -0,0 +1,66 @@
+#include "fluff.hpp"
+
+namespace detail
+{
+  template <typename Fn>
+  class DefaultCase
+  {
+  public:
+    explicit DefaultCase(Fn fn) : fn(fn) {}
+
+    Fn getFn() const { return fn; }
+
+  private:
+    Fn fn;
+  };
+
+  template <typename Result>
+  Result switchAnyImpl(boost::any& a) {
+    assert(false && "No case matched.");
+    std::terminate();
+  }
+
+  template <typename Result, typename Case, typename... Cases>
+  Result switchAnyImpl(boost::any& a, Case c, Cases... cases) {
+    return c.getFn()();
+  }
+}
+
+template <typename Result, typename... Cases>
+Result switchAny(boost::any& a, Cases... cases)
+{
+  return detail::switchAnyImpl<Result>(a, cases...);
+}
+
+template <typename Fn>
+detail::DefaultCase<Fn> defaultCase(Fn fn)
+{
+  return detail::DefaultCase<Fn>(fn);
+}
+
+template <typename Fn>
+detail::DefaultCase<Fn> emptyCase(Fn fn)
+{
+  return detail::DefaultCase<Fn>(fn);
+}
+
+
+
+TEST(SwitchAny, usesDefaultCase)
+{
+	boost::any a;
+	int result = switchAny<int>(a,
+		defaultCase([] { return 1; })
+	);
+	EXPECT_EQ(1, result);
+}
+
+TEST(SwitchAny, usesEmptyCaseForEmpty)
+{
+	boost::any a;
+	int result = switchAny<int>(a,
+		emptyCase([] { return 2; }),
+		defaultCase([] { return 1; })
+	);
+	EXPECT_EQ(2, result);
+}
