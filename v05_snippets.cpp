5945ce26d93a8335b01b405b14005d1bd6ebd95d
CornedBee <sebastian.redl@getdesigned.at>
2015-05-12 09:09:52 -0600

    Add test case for const anys, and snippets to make it work.

---
+++ v05_snippets.cpp
@@ -0,0 +1,23 @@
+template <typename Result, typename... Cases>
+Result switchAny(const boost::any& a, Cases... cases)
+{
+  return detail::switchAnyImpl<Result>(a, a.type(), cases...);
+}
+
+  template <typename Result, typename Any>
+  Result switchAnyImpl(Any& a, const std::type_info& t) {
+
+  template <typename Result, typename Any, typename Case, typename... Cases>
+  Result switchAnyImpl(Any& a, const std::type_info& t,
+                       Case c, Cases... cases) {
+
+    template <typename Any>
+    std::result_of_t<Fn()> call(Any&) { return fn(); }
+
+    template <typename Any>
+    std::result_of_t<Fn()> call(Any&) { return fn(); }
+
+    template <typename Any>
+    std::result_of_t<Fn(T&)> call(Any& a) {
+      return fn(*boost::unsafe_any_cast<T>(&a));
+    }
